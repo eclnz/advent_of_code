902779330e3fcc07c01c994569775f1d3bb6b77d
eclnz <137569906+eclnz@users.noreply.github.com>
2025-12-09 22:19:50 +1300

    2025_7_b passing but too slow

--- 2025_7.jl
+++ 2025_7.jl
@@ -1,41 +1,51 @@
+using BenchmarkTools
+
 function find_splits(line::Vector{Char})
-    line_length = length(line)
-    splits = zeros(Int, line_length)
-    counter = 1
-    @inbounds for index in 1:line_length
+    splits = Int[]
+    @inbounds for index in 1:length(line)
         if line[index] == '^'
-            splits[counter] = index
-            counter += 1
+            push!(splits, index)
         end
     end
     return splits
 end
 
-@assert find_splits(collect(".....^.^.^....."))[1:3] == [6,8,10]
-@assert find_splits(collect("....^.^...^...."))[1:3] == [5,7,11]
+@assert find_splits(collect(".....^.^.^.....")) == [6,8,10]
+@assert find_splits(collect("....^.^...^....")) == [5,7,11]
 beam_positions = [5,7,9,11]
 split_positions = [5,7,11]
 
-function split_beams(new_line::Vector{Char}, beam_indices::Set{Int}, split_counter::Ref{Int}, timelines_counter::Ref{Int})
+function split_beams(new_line::Vector{Char}, beam_indices::Set{Int}, split_counter::Ref{Int})
     split_indices = find_splits(new_line)
-    for s_i in eachindex(split_indices)
-        split_index = split_indices[s_i]
-        if split_index == 0
-            continue
-        end
+    for split_index in split_indices
         if split_index in beam_indices 
             delete!(beam_indices, split_index)
-            if split_index - 1 > 0 
-                push!(beam_indices, split_index - 1)
-            end
-            if split_index + 1 <= length(new_line)
-                push!(beam_indices, split_index + 1)
-            end
+            push!(beam_indices, split_index - 1)
+            push!(beam_indices, split_index + 1)
             split_counter[] += 1
-            timelines_counter[] += 2
-
         end
     end
+end
+
+function split_beams(new_line::Vector{Char}, beam_indices::Vector{Int}, timelines_counter::Ref{Int})
+    split_indices = find_splits(new_line)
+    new_beams = Int[]
+    deleted_indices = Int[]
+    for split_index in split_indices
+        match_indices = findall(x -> x == split_index, beam_indices)
+        if !isempty(match_indices)
+            append!(deleted_indices, match_indices)
+            for _ in 1:length(match_indices)
+                push!(new_beams, split_index - 1)
+                push!(new_beams, split_index + 1)
+                timelines_counter[] += 1
+            end
+        end
+    end
+    for idx in sort(deleted_indices; rev=true)
+        deleteat!(beam_indices, idx)
+    end
+    append!(beam_indices, new_beams)
 end
 
 function find_beam_start_index(start_line::Vector{Char})
@@ -43,16 +53,26 @@ function find_beam_start_index(start_line::Vector{Char})
     return idx === nothing ? 0 : idx
 end
 
-function increment_beam(strings::Vector{String})
+function increment_beam_a(strings::Vector{String})
     string_arrays = [collect(s) for s in strings]
     starting_position = find_beam_start_index(string_arrays[1])
     beam_indices = Set{Int}([starting_position])
     split_counter = Ref(0)
-    timeline_counter = Ref(0)
     for i in 3:2:length(string_arrays)
-        split_beams(string_arrays[i], beam_indices, split_counter, timeline_counter)
+        split_beams(string_arrays[i], beam_indices, split_counter)
     end
     return split_counter[]
+end
+
+function increment_beam_b(strings::Vector{String})
+    string_arrays = [collect(s) for s in strings]
+    starting_position = find_beam_start_index(string_arrays[1])
+    beam_indices = [starting_position]
+    timelines_counter = Ref(1)
+    for i in 3:2:length(string_arrays)
+        split_beams(string_arrays[i], beam_indices, timelines_counter)
+    end
+    return timelines_counter[]
 end
 
 test = 
@@ -75,12 +95,85 @@ test =
     "..............."
 ]
 
-@assert increment_beam(test) == 21
+@assert increment_beam_a(test) == 21
+print(increment_beam_b(test))
+# @assert increment_beam_b(test) == 40
 
-function main()
-    lines = readlines("input/2025_7.txt")
-    lines[end] = replace(lines[end], "\n" => "")
-    print(increment_beam(lines))
+function get_first_index(array::AbstractVector{T}, find_val::T, forward::Bool) where T
+    indices = forward ? (1:length(array)) : (length(array):-1:1)
+    for index in indices
+        if array[index] == find_val
+            return index
+        end
+    end
+    return nothing
 end
 
-main()
+function substrings_from_first_index(s::String)::Vector{Char}
+    chars = collect(s)
+    forward_idx = get_first_index(chars, '^', true)
+    backward_idx = get_first_index(chars, '^', false)
+    return chars[forward_idx:2:backward_idx]
+end
+
+function sanitise_input(strings::Vector{String})
+    n = length(strings)
+    trimmed = strings[3:n-1][1:2:end]
+    substrings = [substrings_from_first_index(s) for s in trimmed]
+    return substrings
+end
+
+function triangle_matrix(triangle::Vector{Vector{Char}})
+    height = length(triangle)
+    width = maximum(length.(triangle))
+    grid = zeros(Int, height, width)
+    for (row_idx, row) in enumerate(triangle)
+        for (col_idx, cell) in enumerate(row)
+            grid[row_idx, col_idx] = cell == '^' ? 1 : 0
+        end
+    end
+    return grid
+end
+
+function pascal_sum(n::Int)::BigInt
+    return big(2)^(n+1) - 2
+end
+
+function pascal_value(row_num::Int, n_from_side::Int)::BigInt
+    k_sym = min(n_from_side - 1, row_num - (n_from_side - 1))
+    n = row_num - 1
+    val = big(1)
+    for i in 1:k_sym
+        val *= (n - i + 1) ÷ i
+    end
+    return val
+end
+
+function find_no_splits(triangle::Vector{Vector{Char}})
+    no_split_positions = Vector{Tuple{Int,Int}}(undef, 0)
+    sizehint!(no_split_positions, sum(length, triangle))
+    for (depth, row) in enumerate(triangle)
+        for (n_from_side, cell) in enumerate(row)
+            if cell == '.'
+                push!(no_split_positions, (n_from_side, depth))
+            end
+        end
+    end
+    return no_split_positions
+end
+
+function get_all_timelines(triangle::Vector{Vector{Char}})
+    total_height = length(triangle[end])
+    total_timelines = pascal_sum(total_height)
+    no_splits = find_no_splits(triangle)
+    for (n_from_side, depth) in no_splits
+        mini_tri_height = total_height - depth + 1
+        sum = pascal_sum(mini_tri_height) * pascal_value(depth, n_from_side)
+        total_timelines -= sum
+    end
+    print(total_timelines)
+end
+
+lines = readlines("input/2025_7.txt")
+lines[end] = replace(lines[end], "\n" => "")
+sanitise_input(test)
