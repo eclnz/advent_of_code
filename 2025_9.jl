213aefa4c3b8d65803bc65007e62ddd80eb8d7bc
eclnz <edward.clarkie@gmail.com>
2025-12-11 12:56:20 +1300

    2025_9_b passing test but wrong solution

--- 2025_9.jl
+++ 2025_9.jl
@@ -1,3 +1,4 @@
+
 function parse_coord(string::String)
     coords = split(string, ",")
     return (parse(Int, coords[1]), parse(Int, coords[2]))
@@ -26,6 +27,41 @@ function get_max_area(strings::Vector{String})
     return(maximum(diff_mat))
 end
 
+function contains_point(p1_i::Int, p2_i::Int, x_coords::Vector{Int}, y_coords::Vector{Int})::Bool
+    p1_x, p1_y = x_coords[p1_i], y_coords[p1_i]
+    p2_x, p2_y = x_coords[p2_i], y_coords[p2_i]
+    for i in eachindex(x_coords)
+        x_coord = x_coords[i]
+        y_coord = y_coords[i]
+        is_within_x = x_coord >= p2_x && x_coord <= p1_x
+        is_within_y = y_coord >= p2_y && y_coord <= p1_y
+        if is_within_x && is_within_y
+            return true
+        end
+    end
+    return false
+end
+
+function get_max_area_b(strings::Vector{String})
+    x_coords,y_coords = parse_coords(strings)
+    diff_map_x = x_coords .- x_coords'
+    diff_map_x .+= 1
+    diff_map_y = y_coords .- y_coords'
+    diff_map_y .+= 1
+    diff_mat = diff_map_x .* diff_map_y
+    mat_size = size(diff_mat, 1)
+    lin_idx_sorted = sortperm(vec(diff_mat); rev=true)
+    x_ind = ((lin_idx_sorted .- 1) .% mat_size) .+ 1
+    y_ind = ((lin_idx_sorted .- 1) .÷ mat_size) .+ 1
+    for i in eachindex(x_ind)
+       point_found = contains_point(x_ind[i], y_ind[i], x_coords, y_coords)
+        if point_found
+            continue
+        end
+        return(diff_mat[x_ind[i], y_ind[i]])
+    end
+end
+
 test = [
     "7,1",  # 1
     "11,1", # 2 <-
@@ -36,8 +72,9 @@ test = [
     "2,3",  # 7 <- 
     "7,3"   # 8
 ]
-result = get_max_area(test)
-@assert result == 50
+@assert get_max_area(test) == 50
+
+@assert get_max_area_b(test) == 24
 
 lines = readlines("input/2025_9.txt")
-print(get_max_area(lines)) # 205.826 μs
+print(get_max_area_b(lines)) # 205.826 μs
