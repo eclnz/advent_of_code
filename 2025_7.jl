aa8def0198ab86794e638267f70c7dc3de5a03af
eclnz <edward.clarkie@gmail.com>
2025-12-10 19:22:13 +1300

    2025_7_b passing and correct.
    Solved the rather challenging problem by instead of keeping track of each new beam, keeping track of how many beams in each index. There is no need to simulate hundres/thousands of the same beams in a single index getting split.
    
    Still a bit of work to be done. Want to make everything index into an array as we know the total max size.

--- 2025_7.jl
+++ 2025_7.jl
@@ -1,3 +1,5 @@
+using BenchmarkTools
+
 function find_splits(line::Vector{Char})
     line_length = length(line)
     splits = zeros(Int, line_length)
@@ -16,7 +18,7 @@ end
 beam_positions = [5,7,9,11]
 split_positions = [5,7,11]
 
-function split_beams(new_line::Vector{Char}, beam_indices::Set{Int}, split_counter::Ref{Int}, timelines_counter::Ref{Int})
+function split_beams_a(new_line::Vector{Char}, beam_indices::Set{Int}, split_counter::Ref{Int})
     split_indices = find_splits(new_line)
     for s_i in eachindex(split_indices)
         split_index = split_indices[s_i]
@@ -25,15 +27,27 @@ function split_beams(new_line::Vector{Char}, beam_indices::Set{Int}, split_count
         end
         if split_index in beam_indices 
             delete!(beam_indices, split_index)
-            if split_index - 1 > 0 
-                push!(beam_indices, split_index - 1)
-            end
-            if split_index + 1 <= length(new_line)
-                push!(beam_indices, split_index + 1)
-            end
+            push!(beam_indices, split_index - 1)
+            push!(beam_indices, split_index + 1)
             split_counter[] += 1
-            timelines_counter[] += 2
+        end
+    end
+end
 
+function split_beams_b(new_line::Vector{Char}, beam_indices::Set{Int}, n_timelines::Vector{Int})
+    split_indices = find_splits(new_line)
+    for s_i in eachindex(split_indices)
+        split_index = split_indices[s_i]
+        if split_index == 0
+            continue
+        end
+        if split_index in beam_indices
+            delete!(beam_indices, split_index)
+            push!(beam_indices, split_index - 1)
+            push!(beam_indices, split_index + 1)
+            n_timelines[split_index + 1] += n_timelines[split_index]
+            n_timelines[split_index - 1] += n_timelines[split_index]
+            n_timelines[split_index] = 0
         end
     end
 end
@@ -43,16 +57,28 @@ function find_beam_start_index(start_line::Vector{Char})
     return idx === nothing ? 0 : idx
 end
 
-function increment_beam(strings::Vector{String})
+function increment_beam_a(strings::Vector{String})
     string_arrays = [collect(s) for s in strings]
     starting_position = find_beam_start_index(string_arrays[1])
     beam_indices = Set{Int}([starting_position])
     split_counter = Ref(0)
-    timeline_counter = Ref(0)
     for i in 3:2:length(string_arrays)
-        split_beams(string_arrays[i], beam_indices, split_counter, timeline_counter)
+        split_beams_a(string_arrays[i], beam_indices, split_counter)
     end
     return split_counter[]
+end
+
+function increment_beam_b(strings::Vector{String})
+    string_arrays = [collect(s) for s in strings]
+    starting_position = find_beam_start_index(string_arrays[1])
+    beam_indices = Set{Int}([starting_position])
+    n_timelines = zeros(Int,length(string_arrays[1]))
+    middle_index = Int(length(string_arrays) / 2)
+    n_timelines[middle_index] = 1
+    for i in 3:2:length(string_arrays)
+        split_beams_b(string_arrays[i], beam_indices, n_timelines)
+    end
+    return sum(n_timelines)
 end
 
 test = 
@@ -75,12 +101,16 @@ test =
     "..............."
 ]
 
-@assert increment_beam(test) == 21
+@assert increment_beam_a(test) == 21
+@assert increment_beam_b(test) == 40
 
 function main()
     lines = readlines("input/2025_7.txt")
     lines[end] = replace(lines[end], "\n" => "")
-    print(increment_beam(lines))
+    print(increment_beam_a(lines))
+    print(" ")
+    print(increment_beam_b(lines))
+    @btime increment_beam_b($lines)
 end
 
 main()
